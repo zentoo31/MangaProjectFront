7aa3b11310480bd70a20a58a7b3e7697699d03ea
zentoo31 <zentoo31@gmail.com>
2024-07-25 22:32:09 -0500

    second commit

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  mode: number = 0;
   
+
+  actualizarBotonUI(){
+    this.mode = (this.mode + 1) % 2;
+  }
+
 }
