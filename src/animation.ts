b45f66eb367f21657d913f397e360d72c810d89d
zentoo31 <zentoo31@gmail.com>
2024-08-09 16:52:09 -0500

    9/08/24

--- src/animation.ts
+++ src/animation.ts
@@ -1,8 +1,7 @@
 import { trigger, transition, style, animate, query } from '@angular/animations';
-
 export const fadeAnimation = trigger('fadeAnimation', [
     transition('* <=> *', [
       style({ opacity: 0 }),
       animate('500ms', style({ opacity: 1 }))
     ])
-  ]);
+]);
