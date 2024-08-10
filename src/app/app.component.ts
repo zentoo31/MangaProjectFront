5f15559be0032efdd3a568429acdd4f64828f34c
zentoo31 <zentoo31@gmail.com>
2024-08-10 15:10:53 -0500

    10/08/24

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, NavigationStart, Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { fadeAnimation } from '../animation';
 import { SidebarComponent } from './sidebar/sidebar.component';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -15,20 +15,39 @@ import { SidebarComponent } from './sidebar/sidebar.component';
   animations: [fadeAnimation]
 })
 
-export class AppComponent {
+export class AppComponent{
   mode: number = 0;
+
+  constructor(private router: Router){}
 
   reciviedNumber(value: number){
     this.mode = value;
     this.updateMode();
   }
-
+  
   updateMode(){
     if (this.mode == 1) {
-      document.body.setAttribute('data-bs-theme','light');
+      globalThis.document.body.setAttribute('data-bs-theme','light');
     }else{
-      document.body.setAttribute('data-bs-theme','dark');
+      globalThis.document.body.setAttribute('data-bs-theme','dark');
     }
+  }
+  
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      const progressBar = globalThis.document.querySelector('.progress-bar') as HTMLElement;
+      
+      if (event instanceof NavigationStart) {
+        progressBar.style.width = '0%';
+        progressBar.style.transition = 'width 0.4s ease';
+        setTimeout(() => progressBar.style.width = '50%', 100);  // Simula un proceso de carga inicial
+      }
+      
+      if (event instanceof NavigationEnd) {
+        setTimeout(() => progressBar.style.width = '100%', 400); // Completa la barra de progreso
+        setTimeout(() => progressBar.style.width = '0%', 600);   // Resetea la barra después de un breve tiempo
+      }
+    });
   }
   
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,8 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideAnimations()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideAnimations(), provideHttpClient()]
 };
